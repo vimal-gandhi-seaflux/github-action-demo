3b82e50b28f0375981e4c60b5361b94f6addbb55
vimal gandhi <vimal.gandhi@seaflux.tech>
2023-10-19 17:21:54 +0530

    feat : set up project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
+          DevOps Pro
+        </p>
+        <a
+          className="App-link"
+          href="https://youtube.com/@DevOpsPro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn DevOps
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
